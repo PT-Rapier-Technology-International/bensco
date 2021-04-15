83ae5f7533691af0f5e5e2aa3c55f042eccc0063
novaari2 <novaari20@gmail.com>
2021-04-15 10:50:00 +0700

    setting untuk run di lokal

--- config/config.php
+++ config/config.php
@@ -48,7 +48,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 */
 
-$config['base_url']	= "https://".$_SERVER["HTTP_HOST"].str_replace(basename($_SERVER["SCRIPT_NAME"]),"",$_SERVER["SCRIPT_NAME"]);
+// $config['base_url']	= "https://".$_SERVER["HTTP_HOST"].str_replace(basename($_SERVER["SCRIPT_NAME"]),"",$_SERVER["SCRIPT_NAME"]);
+$config['base_url'] = 'http://localhost:8888/public_html/';
 
 
 
